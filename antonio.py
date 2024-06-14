cd0e05ca1d46c1848679c0aefff17b0408f6ae8b
prozdsudi@gmail.com <prozdsudi@gmail.com>
2024-06-13 21:45:41 -0300

    ola

--- antonio.py
+++ antonio.py
@@ -1,6 +1,6 @@
 nome = input("Digite o seu nome :")
 idade = int(input("Digite a sua idade:"))
 altura = float(input("Digite a sua altura:"))
-peso = float(input("Digite o seu peso :"))
+peso = int(input("Digite o seu peso :"))
 
-print(f(O nome do usuario é [nome] sua é [idade],a [altura] e o peso é [peso]))
+print (f"O nome do usuario é {nome} sua é {idade}anos,a altura {altura} e o peso é {peso}")
