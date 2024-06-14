6cee7f26c4e6af9b026fe09174bbac1ab58330b9
victor-negocios <victor_hgalves@hotmail.com>
2024-06-13 21:22:07 -0300

    aula

---
+++ antonio.py
@@ -0,0 +1,6 @@
+nome = input("Digite o seu nome :")
+idade = int(input("Digite a sua idade:"))
+altura = float(input("Digite a sua altura:"))
+peso = float(input("Digite o seu peso :"))
+
+print(f(O nome do usuario é [nome] sua é [idade],a [altura] e o peso é [peso]))
