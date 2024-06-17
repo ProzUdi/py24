249e09dbc9a928c811030d7e9e3253ac1a34a8e7
prozdsudi@gmail.com <prozdsudi@gmail.com>
2024-06-17 20:10:49 -0300

    definindo funções

--- Patrik.py
+++ Patrik.py
@@ -1,1 +1,15 @@
-print("Meu programa no git")
+def saudacao():
+    print("Olá mundo")
+    print("Esta é uma função")
+
+saudacao() #<< CHAMANDO A FUNÇÃO<<
+
+def somar(num1, num2):
+    soma = num1 + num2
+    return(soma)
+
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: " ))
+
+s = somar(numero1, numero2)
+print(s)
