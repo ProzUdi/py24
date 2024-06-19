220c0d0c10ef2bb8bea4b572ab20ffeb443d6391
prozdsudi@gmail.com <prozdsudi@gmail.com>
2024-06-19 20:30:55 -0300

    contatos csv

--- contatos.py
+++ contatos.py
@@ -1,7 +1,16 @@
-contatos = open("contatos.txt", "r")
+contatos = open("contatos.csv", "r")
 linha = contatos.readline()
 while linha != "":
     print(linha)
     linha = contatos.readline()
 
 contatos.close()    
+
+#Esse é em TXT ORIGINAL<<
+#contatos = open("contatos.txt", "r")
+#linha = contatos.readline()
+##while linha != "":
+  #  print(linha)
+   # linha = contatos.readline()
+
+#contatos.close()    
