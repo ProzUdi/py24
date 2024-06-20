ca070e49131fd6681b06621115b62def8991e0f0
dielsondiniz <dielsondiniz123@gmail.com>
2024-06-19 21:13:40 -0300

    Leitura de arquivo

--- funcoes.py
+++ funcoes.py
@@ -15,9 +15,39 @@ def dividir(num1, num2):
   return num1 / num2
 
 def fatorial(num):
-  fat = 1
-  while num > 1:
-    fat = fat * num
-    num = num - 1
+  if (num == 1):
+    return 1
+  
+  return num * fatorial(num -1)
 
-  return fat
+def opcoes():
+  print("1 - Ler contatos")
+  print("2 - Adicionar Contato")
+  print("3 - Sair")
+  opcao = int(input())
+  return opcao
+
+def lerContatos():
+  arquivo = open("contatos.csv", "r")
+  linha = arquivo.readline()
+
+  contatos = []
+  while linha != "":
+    contato = {}
+    items = linha.split(";")
+    contato["nome"] = items[0]
+    contato["telefone"] = items[1].replace("\n","")
+    contatos.append(contato)
+    linha = arquivo.readline()
+  arquivo.close()
+  return contatos
+
+def addContato():
+  contato = {}
+  contato["nome"] = input("Digite o nome: ")
+  contato["telefone"] = input("Digite o telefone: ")
+  arquivo = open("contatos.csv", "a")
+  arquivo.write(contato["nome"]+";"+contato["telefone"])
+  arquivo.close()
+  return contato
+
