b1e61acfcd9c4a5875fd8c776b42e88f463658d9
dielsondiniz <dielsondiniz123@gmail.com>
2024-06-25 19:47:33 -0300

    Exclusão e Edição de contatos

--- funcoesContato.py
+++ funcoesContato.py
@@ -1,7 +1,9 @@
 def opcoes():
   print("1 - Ler contatos")
   print("2 - Adicionar Contato")
-  print("3 - Sair")
+  print("3 - Editar Contato")
+  print("4 - Excluir Contato")
+  print("5 - Sair")
   opcao = int(input())
   return opcao
 
@@ -28,5 +30,54 @@ def addContato():
   arquivo.write("\n"+contato["nome"]+";"+contato["telefone"])
   arquivo.close()
   return contato
+
+def editarContato(contatos):
+  contatoEdicao = {}
+  contatoEdicao["nome"] = input("Digite o nome que deseja editar: ")
+  contador = 0
+  encontrado = False
+  for contato in contatos:
+    if (contatoEdicao["nome"] == contato["nome"]):
+      encontrado = True
+      print("O numero atual é: ",contato["telefone"])
+      contatoEdicao["telefone"] = input("Digite o novo numero do telefone: ")
+      contatos[contador] = contatoEdicao
+    contador +=1
+
+  if (encontrado):
+    arquivo = open("contatos.csv", "w")
+    for contato in contatos:
+      arquivo.write(contato["nome"]+";"+contato["telefone"]+"\n")
+
+    arquivo.close()
+  else:
+    print("Contato não encontrado")
+
+  return contatos
+
+def excluirContato(contatos):
+  contatoEdicao = {}
+  contatoEdicao["nome"] = input("Digite o nome que deseja excluir: ")
+  contador = 0
+  encontrado = False
+  for contato in contatos:
+    if (contatoEdicao["nome"] == contato["nome"]):
+      encontrado = True
+      print("Realmente deseja excluir o numero (s/n): ",contato["telefone"])
+      resposta = input()
+      if (resposta == "s"):
+        contatos.pop(contador)
+    contador +=1
+
+  if (encontrado):
+    arquivo = open("contatos.csv", "w")
+    for contato in contatos:
+      arquivo.write(contato["nome"]+";"+contato["telefone"]+"\n")
+
+    arquivo.close()
+  else:
+    print("Contato não encontrado")
+
+  return contatos
 
 
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import funcoesContato as funcoes
-
 contatos = funcoes.lerContatos()
-
 opcao = 0
-while opcao != 3:
+while opcao != 5:
   opcao = funcoes.opcoes()
   if(opcao == 1):
     print("Nome - Telefone")
@@ -12,6 +10,10 @@ while opcao != 3:
   elif(opcao == 2):
     contatos.append(funcoes.addContato())
   elif(opcao == 3):
+    funcoes.editarContato(contatos)
+  elif(opcao == 4):
+    funcoes.excluirContato(contatos)
+  elif(opcao == 5):
     print("Obrigado por usar o sistema")
   else:
     print("Opção invalida")
